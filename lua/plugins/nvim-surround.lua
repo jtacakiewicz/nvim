b976f1cfeebb0f49d1ac8b8c476e351d44525fc6
Epim3dium <Epim3dium@protonmail.com>
2024-11-04 23:48:32 +0000

    added surrund

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,17 @@
+local status, surround = pcall(require, "nvim-surround")
+if not status then return end
+surround.setup{
+    keymaps = {
+        insert = "<C-s>",
+        insert_line = "<C-s>",
+        normal = "<C-s>",
+        normal_cur = "<C-s>s",
+        normal_line = "<C-s>S",
+        normal_cur_line = "<C-S>SS",
+        visual = "<C-S>",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
+    },
+}
